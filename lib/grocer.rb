845fa127047e76c1c952c23ac47fde007741546e
logicfirst <slee@logicfirstllc.com>
2020-11-05 14:03:22 -0500

    commit tree 56f16395757ecc503990d9bb03673684a2672ba9

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,39 +1,39 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  count = 0
-  while count < collection.length do 
-    if collection[count][:item] == name
-      return collection[count]
+  counter = 0
+  while counter < collection.length do 
+    if name == collection[counter][:item]
+       return collection[counter]
+    else
+       nil
     end
-    count += 1
+    counter += 1
   end
 end
 
-def consolidate_cart(cart)
+def consolidate_cart(cart)  
+  # REMEMBER: This returns a new Array that represents the cart. Don't merely
+  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   counter = 0
   new_cart = []
 
   while counter < cart.length do
-    new_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
-    if new_item 
-      new_item[:count] += 1
+    next_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    if next_item
+      next_item[:count] +=1
     else
-      new_item = {
-        :item => cart[counter][:item],
-        :price => cart[counter][:price],
-        :clearance => cart[counter][:clearance],
-        :count => 1
+      next_item = {
+        item: cart[counter][:item],
+        price: cart[counter][:price],
+        clearance: cart[counter][:clearance],
+        count: 1
       }
-      new_cart << new_item
+      new_cart << next_item
     end
     counter += 1
   end
   new_cart
 end
-
-
-
-
 
   
